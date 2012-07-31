0c60d43dfc216684e9d585bf3be3d3f583b04e01
William Giffin <masterchief_whg@woh.rr.com>
2012-07-31 10:50:41 -0400

    this is the AVATAR2 code

---
+++ AVATAR2/src/sate2012/avatar/android/Constants.java
@@ -0,0 +1,9 @@
+package sate2012.avatar.android;
+
+public class Constants {
+	public static final String STORAGE_DIRECTORY = "/Blueprint_Storage/";
+	public static final String MEDIA_DIRECTORY = "media/";
+	public static final int CAMERA_REQUEST = 3;
+	public static final int VOICE_REQUEST = 1;
+	public static final int VIDEO_REQUEST = 2;
+}
